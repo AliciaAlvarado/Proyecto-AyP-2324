331e9c0c0730c19590d4bb8d6013a02aebe66d9c
AliciaAlvarado <alicia.alvarado@correo.unimet.edu.ve>
2024-08-07 18:56:54 -0400

    update planeta class

--- planeta.py
+++ planeta.py
@@ -38,3 +38,44 @@ class Planeta:
                 f"URL: {self.url}\n"
                 f"Creado: {self.creado}\n"
                 f"Editado: {self.editado}\n")
+
+def cargar_nombres_de_urls(urls):
+    nombres = []
+    for url in urls:
+        respuesta = requests.get(url)
+        if respuesta.status_code == 200:
+            datos = respuesta.json()
+            nombres.append(datos['result']['properties']['name'])
+    return nombres
+
+def cargar_planetas():
+    url_api = "https://www.swapi.tech/api/planets"
+    while url_api:
+        respuesta = requests.get(url_api)
+        datos = respuesta.json()
+        for planeta in datos['results']:
+            detalles_respuesta = requests.get(planeta['url'])
+            detalles = detalles_respuesta.json()['result']['properties']
+            
+            p = Planeta(
+                nombre=detalles['name'],
+                diametro=detalles['diameter'],
+                periodo_rotacion=detalles['rotation_period'],
+                periodo_orbita=detalles['orbital_period'],
+                gravedad=detalles['gravity'],
+                poblacion=detalles['population'],
+                clima=detalles['climate'],
+                terreno=detalles['terrain'],
+                agua_superficial=detalles['surface_water'],
+                residentes=cargar_nombres_de_urls(detalles['residents']) if 'residents' in detalles else [],
+                peliculas=cargar_nombres_de_urls(detalles['films']) if 'films' in detalles else [],
+                url=detalles['url'],
+                creado=detalles['created'],
+                editado=detalles['edited']
+            )
+            Planeta.lista_planetas.append(p)
+        
+        url_api = datos.get('next')
+
+if __name__ == "__main__":
+    cargar_planetas()
