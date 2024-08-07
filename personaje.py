1640e0bfc741a6acca6a9a2988faf409c4cd4c62
AliciaAlvarado <alicia.alvarado@correo.unimet.edu.ve>
2024-08-06 23:15:20 -0400

    update personaje class 2

--- personaje.py
+++ personaje.py
@@ -51,3 +51,42 @@ def cargar_nombres_de_urls(urls):
             datos = respuesta.json()
             nombres.append(datos['result']['properties']['name'])
     return nombres
+
+def cargar_personajes():
+    url_api = "https://www.swapi.tech/api/people"
+    while url_api:
+        respuesta = requests.get(url_api)
+        datos = respuesta.json()
+        for personaje in datos['results']:
+            detalles_respuesta = requests.get(personaje['url'])
+            detalles = detalles_respuesta.json()['result']['properties']
+            
+            planeta_origen = None
+            if detalles['homeworld']:
+                planeta_respuesta = requests.get(detalles['homeworld'])
+                planeta_origen = planeta_respuesta.json()['result']['properties']['name']
+
+            especies = cargar_nombres_de_urls(detalles['species']) if 'species' in detalles else ["Desconocido"]
+            especie = especies[0] if especies else "Desconocido"
+
+            p = Personaje(
+                nombre=detalles['name'],
+                nacimiento=detalles['birth_year'],
+                color_ojos=detalles['eye_color'],
+                genero=detalles['gender'],
+                color_cabello=detalles['hair_color'],
+                altura=detalles['height'],
+                peso=detalles['mass'],
+                color_piel=detalles['skin_color'],
+                planeta_origen=planeta_origen,
+                peliculas=cargar_nombres_de_urls(detalles['films']) if 'films' in detalles else [],
+                naves=cargar_nombres_de_urls(detalles['starships']) if 'starships' in detalles else [],
+                vehiculos=cargar_nombres_de_urls(detalles['vehicles']) if 'vehicles' in detalles else [],
+                especie=especie,
+                url=detalles['url'],
+                creado=detalles['created'],
+                editado=detalles['edited']
+            )
+            Personaje.lista_personajes.append(p)
+        
+        url_api = datos.get('next')
